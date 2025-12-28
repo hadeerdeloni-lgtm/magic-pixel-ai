1202ce19ebae449f31b0c5f766efdccd211a884d
hadeerdeloni-lgtm <hadeerdeloni@gmail.com>
2025-12-28 21:21:05 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,16 @@
-
 import streamlit as st
+from huggingface_hub import InferenceClient
 
-st.title("Test Image")
+st.title("Magic Pixel AI")
 
-st.image("https://picsum.photos/400")
+client = InferenceClient(
+    model="stabilityai/stable-diffusion-xl-base-1.0",
+    token=st.secrets["HF_TOKEN"]
+)
+
+prompt = st.text_input("Describe your image:")
+
+if st.button("Generate Image"):
+    with st.spinner("Generating..."):
+        image = client.text_to_image(prompt)
+        st.image(image)
